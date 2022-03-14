b62310b02b6e9c3ad99011bce6246a9d953d4c7a
cguerrero73 <cristian_h_guerrero@hotmail.com>
2022-03-14 10:28:39 -0600

    certificado

--- apps/api/src/app/database/database.service.ts
+++ apps/api/src/app/database/database.service.ts
@@ -5,11 +5,14 @@ import { ConfigModule } from '../config/config.module';
 import { ConfigService } from '../config/config.service';
 import { CustomNamingStrategy } from '../config/CustomNamingStrategy';
 
+import * as fs from 'fs';
+
 export const databaseProviders = [
   TypeOrmModule.forRootAsync({
     imports: [ConfigModule],
     inject: [ConfigService],
     async useFactory(config: ConfigService) {
+
       return {
         type: 'postgres',
         host: config.get(Configuration.DB_HOST),
@@ -17,6 +20,10 @@ export const databaseProviders = [
         username: config.get(Configuration.DB_USERNAME),
         password: config.get(Configuration.DB_PASSWORD),
         port: config.get(Configuration.DB_PORT),
+        ssl: {
+          rejectUnauthorized: false,
+          ca: fs.readFileSync( __dirname + '/assets/ca-certificate.crt').toString(),
+        },
         entities: getMetadataArgsStorage().tables.map((tbl) => tbl.target),
         migrations: [__dirname + '/migrations/*{.ts,.js}'],
         logging: true,
