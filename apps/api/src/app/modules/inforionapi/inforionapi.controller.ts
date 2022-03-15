41167eac95faea34ba4ea466f6ba2f661184b290
cguerrero73 <cristian_h_guerrero@hotmail.com>
2022-03-15 00:14:35 -0600

    parser

--- apps/api/src/app/modules/inforionapi/inforionapi.controller.ts
+++ apps/api/src/app/modules/inforionapi/inforionapi.controller.ts
@@ -24,14 +24,39 @@ export class InforionapiController {
     if (ROOT.syncmaintenanceorder) {
 
 
-      const org = ROOT.syncmaintenanceorder.dataarea[0].maintenanceorder[0].maintenanceorderheader[0].documentid[0].id[0].$.accountingEntity;
+const wrk_tenant= ROOT.syncmaintenanceorder.dataarea[0].sync[0].tenantid[0];
+const wrk_org=ROOT.syncmaintenanceorder.dataarea[0].maintenanceorder[0].maintenanceorderheader[0].documentid[0].id[0].$.accountingEntity;
+const wrk_code=ROOT.syncmaintenanceorder.dataarea[0].maintenanceorder[0].maintenanceorderheader[0].documentid[0].id[0]._;
+const wrk_status=ROOT.syncmaintenanceorder.dataarea[0].maintenanceorder[0].maintenanceorderheader[0].status[0].reasoncode[0];
+const wrk_rstatus='x';
+
+const classification = ROOT.syncmaintenanceorder.dataarea[0].maintenanceorder[0].maintenanceorderheader[0].classification;
+let wrk_type = '';
+classification.forEach(element => {
+  if (element.codes[0].code[0].$.listID === 'WorkType'){
+     wrk_type=element.codes[0].code[0]._;
+  }
+  
+});
+
+// const wrk_priority=;
+// const wrk_criticality=;
+// const wrk_mrc=;
+// const wrk_planned_hours=;
+// const wrk_created=;
+// const wrk_reported=;
+// const wrk_completed=;
+// const wrk_start_sched=;
+// const wrk_end_sched=;
+// const wrk_desc=;
+// const wrk_equip=;
+// const wrk_equip_org=;
+// const wrk_person=;
 
 
-      console.log('BOD is a maintenanceorder');
-      console.log('Org:', org);
+      console.log('BOD is a maintenanceorder', wrk_type);
 
 
-      ROOT.syncmaintenanceorder
     }
 
     if (body.employeeworktime) {
